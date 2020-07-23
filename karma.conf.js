a5569e0f4191d62b47d3b8bd31a676631dcd798b
Henna <62267575+HennaAbbas@users.noreply.github.com>
2020-07-23 16:11:33 -0700

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: true,
+//     singleRun: true,
     restartOnFileChange: true
 //     browserNoActivityTimeout: 60000
   });
